af74cd37823d748db9e0f0512d3e995ca05b95e6
ADEP-123 <andep210399@gmail.com>
2024-04-15 12:15:28 -0500

    feat: :sparkles: recibiendo y convirtiendo informacion del usuario

--- scripts/index.js
+++ scripts/index.js
@@ -3,16 +3,39 @@ import Variables from "./variables.js"
 document.addEventListener("DOMContentLoaded", e => {
     const variables = new Variables
 
+    // Event to deploy and hide modal window about how to use the APP
     variables.infoButt.addEventListener("click", e => {
         e.preventDefault()
         e.stopPropagation()
         const modal = variables.infoAPP
-        console.log(modal);
+        // console.log(modal);
         if (modal.style.right === "-100%" || modal.style.right === "") {
             modal.style.right = "0%"
-        }else{
+        } else {
             modal.style.right = "-100%"
         }
+    })
+
+    // Event to recolect user info and translate it
+    variables.calcButton.addEventListener("click", e => {
+        e.preventDefault()
+        e.stopPropagation()
+        variables.setUserEq(variables.userEqInput.value.toLowerCase())
+        try {
+            // Interpretar la ecuación utilizando Math.js
+            const userFunction = math.compile(variables.userEq);
+
+            const result = userFunction.evaluate({ x: 1 });
+
+            console.log({
+                Value: variables.userEq,
+                Result: result
+            });
+        } catch (error) {
+            variables.setNewErrorInfo("Error en la escritura de la ecuacion");
+            console.error("Error en la escritura de la ecuacion");
+        }
+
     })
 
 })
--- scripts/variables.js
+++ scripts/variables.js
@@ -4,8 +4,20 @@ class Variables {
             this.infoAPP = document.querySelector("#infoModal")
             this.infoButt = document.querySelector("#infoButt")
             Variables.instance = this;
+            this.userEqInput = document.querySelector("#userEq")
+            this.userEq = "";
+            this.calcButton = document.querySelector("#calcul")
+            this.errInfo = "";
         }
         return Variables.instance
+    }
+
+    setUserEq(value) {
+        this.userEq = value;
+    }
+
+    setNewErrorInfo(info) {
+        this.errInfo = info;
     }
 
 }
