4687f9e9a8c4c574e1a5e4c99422d595e0a70270
ADEP-123 <andep210399@gmail.com>
2024-04-15 19:10:06 -0500

    fix: :bug: arreglando error de ifs

--- scripts/modules/readTransEq/calByIter.js
+++ scripts/modules/readTransEq/calByIter.js
@@ -1,38 +1,45 @@
 const calByIter = (userFunction, limInf, limSup, cantDec, cantIter) => {
     let rows = []
-    // const result = userFunction.evaluate({ x: 1 });
-    // console.log({
-    //     Value: userEq,
-    //     Result: result
-    // });
+
     let iteration; let xi; let xu; let xr; let fxi; let fxu; let fxr; let sigfxi; let sigfxu; let sigfxr; let cambio; let error; let resAbs
     for (let i = 0; i < cantIter; i++) {
         iteration = i + 1;
+        let oldXr;
         if (iteration == 1) {
             xi = limInf;
             xu = limSup;
-        } else {
+            resAbs = ""
+        }
+        if (iteration != 1) {
+            oldXr = xr;
             if (sigfxi === sigfxr) {
-                xi = xr;
-                cambio = /*html*/`<p>x<sub>i</sub> → x<sub>${iteration}</sub></p>`
+                xi = xr
             } else if (sigfxu === sigfxr) {
-                xu = xr;
-                cambio = /*html*/`<p>x<sub>u</sub> → x<sub>${iteration}</sub></p>`
-            } else {
-                console.error("La ecuacion no tiene raiz");
-                return;
+                xu = xr
             }
         }
         fxi = userFunction.evaluate({ x: xi });
         fxu = userFunction.evaluate({ x: xu });
-        const oldXr = xr;
         xr = ((xi * fxu) - (xu * fxi)) / (fxu - fxi);
         fxr = userFunction.evaluate({ x: xr });
         fxi > 0 ? sigfxi = "+" : sigfxi = "-";
         fxu > 0 ? sigfxu = "+" : sigfxu = "-";
         fxr > 0 ? sigfxr = "+" : sigfxr = "-";
+        if (sigfxi === sigfxr) {
+            cambio = /*html*/`<p>x<sub>i</sub> → x<sub>${iteration}</sub></p>`
+        } else if (sigfxu === sigfxr) {
+            cambio = /*html*/`<p>x<sub>u</sub> → x<sub>${iteration}</sub></p>`
+        } else if (sigfxi === sigfxr && sigfxu === sigfxr) {
+            console.error("La ecuacion no tiene raiz");
+            return;
+        } else if (sigfxi != sigfxr && sigfxu != sigfxr) {
+            console.error("La ecuacion no tiene raiz");
+            return
+        }
+        if (iteration != 1) {
+            resAbs = Math.abs(xr - oldXr)
+        }
         error = fxr;
-        iteration == 1 ? resAbs = "" : resAbs = (Math.abs(xr - oldXr));
 
         rows.push({
             iteration,
